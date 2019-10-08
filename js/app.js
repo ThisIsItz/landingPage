628057c33578cb82b6945e73949a1a058f2a7ee0
ThisIsItz <ItziarUnderTheSea@gmail.com>
2019-10-08 16:55:02 +0100

    Adding some functions

--- js/app.js
+++ js/app.js
@@ -13,25 +13,6 @@
  * 
 */
 
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 
 // build the nav
 
@@ -58,6 +39,26 @@ createList();
 // Add class 'active' to section when near top of viewport
 
 
+function elementFromTop(elem, classToAdd, distanceFromTop, unit) {
+    var winY = window.innerHeight || document.documentElement.clientHeight,
+    elemLength = elem.length, distTop, distPercent, distPixels, distUnit, i;
+    for (i = 0; i < elemLength; ++i) {
+        distTop = elem[i].getBoundingClientRect().top;
+        distPercent = Math.round((distTop / winY) * 100);
+        distPixels = Math.round(distTop);
+        distUnit = unit == 'percent' ? distPercent : distPixels;
+        if (distUnit <= distanceFromTop) {
+           if (!hasClass(elem[i], classToAdd)) { addClass(elem[i], classToAdd); }
+           } else {
+           delClass(elem[i], classToAdd);
+           }
+        }
+}
+// params: element id, class to add, distance from top, unit ('percent' or 'pixels')
+
+elementFromTop('allSections', 'your-active-class', '20', 'px');
+
+
 // Scroll to anchor ID
 
 const allAnchors = document.querySelectorAll('a');
@@ -80,6 +81,42 @@ for (let i = 0; i < list.length; i++){
  * Begin Events
  * 
 */
+
+
+
+//let hideButton = document.querySelectorAll('.active');
+//let inputButton = document.querySelectorAll('input');
+//
+//
+//    
+//    for (let i = 0; i < hideButton.length; i++){
+//        let content = document.querySelector('.text' + i);
+//        content.style.display = "block";
+//            if (content.style.display=="block"){
+//                console.log('block');
+//            }else{
+//                console.log('block');
+//            }
+//        
+//        
+//        
+//            hideButton[i].addEventListener('click', function(){
+//                console.log("desactivar");
+//                content.style.display = "none"
+//                hideButton[i].className = "unactive";
+//                hideButton[i].value = "Show";
+//            });
+//    }
+//
+//    
+
+
+
+//hideButton.addEventListener('click', function(){
+//   console.log('button');
+//    contentText.style
+//    
+//});
 
 // Build menu 
 
